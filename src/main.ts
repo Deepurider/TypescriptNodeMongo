5ce35909ad6a353c2120088c90f14d85fcb4cd81
Deep Patel <pateldeep691996@gmail.com>
2024-01-09 22:25:12 +0530

    Add code for nested child data.

--- src/main.ts
+++ src/main.ts
@@ -25,24 +25,44 @@ adminRoutes.get("/api/user", async (req, res) => {
 
 adminRoutes.put("/api/user", async (req, res) => {
   const { firstName, lastName, id } = req.body;
+  // const userId = "659d7627fad7d552d967e766";
+  // const contactId = "659d7627fad7d552d967e767";
+  // const addressId = "659d7627fad7d552d967e768";
+  // const updateData = "This is updated Data";
+
   const users = await User.find({ _id: id }).updateOne(
     {},
     { firstName: firstName, lastName: lastName }
   );
+
+  // update data with nested child node.
+  // const updatedUser = await User.updateOne(
+  //   {
+  //     _id: userId,
+  //     "contacts._id": contactId,
+  //     "contacts.addresses._id": addressId,
+  //   },
+  //   {
+  //     $set: {
+  //       "contacts.$.addresses.$[addr].addressLine1": "This is address line 2",
+  //       "contacts.$.addresses.$[addr].addressLine2": "This is address line 1",
+  //     },
+  //   },
+  //   {
+  //     arrayFilters: [
+  //       {
+  //         "addr._id": addressId,
+  //       },
+  //     ],
+  //   }
+  // );
   return res.json(users);
 });
 
 adminRoutes.post("/api/user", async (req, res) => {
   const { firstName, lastName } = req.body;
-  const user = User.build({
-    firstName: firstName,
-    lastName: lastName,
-  });
-  await user.save();
-  return res.json({
-    firstName: firstName,
-    lastName: lastName,
-  });
+  const user = await User.build(req.body).save();
+  return res.json(user);
 });
 
 adminRoutes.delete("/api/user", async (req, res) => {
--- src/model/user.ts
+++ src/model/user.ts
@@ -3,18 +3,59 @@ import mongoose from "mongoose";
 interface IUser {
   firstName: string;
   lastName: string;
+  contacts: IContact[];
+}
+
+interface IContact {
+  title: string;
+  addresses: IAddress[];
+}
+
+interface IAddress {
+  addressLine1: string;
+  addressLine2: string;
 }
 
 interface UserDoc extends mongoose.Document {
   firstName: string;
   lastName: string;
+  contacts: ContactDoc[];
+}
+
+interface ContactDoc extends mongoose.Document {
+  title: string;
+  addresses: AddressDoc[];
+}
+
+interface AddressDoc extends mongoose.Document {
+  addressLine1: string;
+  addressLine2: string;
 }
 
 interface IStudentModel extends mongoose.Model<UserDoc> {
   build(user: IUser): UserDoc;
 }
 
-const userSchema = new mongoose.Schema({
+const addressSchema = new mongoose.Schema<IAddress>({
+  addressLine1: {
+    type: String,
+    required: false,
+  },
+  addressLine2: {
+    type: String,
+    required: false,
+  },
+});
+
+const contactSchema = new mongoose.Schema<IContact>({
+  addresses:[addressSchema],
+  title: {
+    type: String,
+    required: false
+  }
+});
+
+const userSchema = new mongoose.Schema<IUser>({
   firstName: {
     type: String,
     require: false,
@@ -23,10 +64,12 @@ const userSchema = new mongoose.Schema({
     type: String,
     require: false,
   },
+  contacts:[contactSchema]
 });
 
 userSchema.statics.build = (user: IUser) => {
+  console.log(user);
   return new User(user);
 };
-const User = mongoose.model<UserDoc, IStudentModel>("User", userSchema);
+const User = mongoose.model<any, IStudentModel>("User", userSchema);
 export { User };
